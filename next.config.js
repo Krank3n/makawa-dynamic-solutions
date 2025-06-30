3530c7c8a93567f0198a51ed2a010101c8b6c94c
Thomas A Hansen <45204791+Krank3n@users.noreply.github.com>
2025-06-30 20:09:52 +1000

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Also disables ESLint errors during build
+  },
   images: {
     domains: [
       'avatars.githubusercontent.com',
-  ],
+    ],
     remotePatterns: [
       {
         protocol: 'https',
